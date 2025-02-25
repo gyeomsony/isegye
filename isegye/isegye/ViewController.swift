daafbf17870489b24710ba9b63506543b045c880
sonny <gyeomsony@gmail.com>
2025-02-25 22:52:15 +0900

    ✨[feat] 기본 셋팅

---
+++ isegye/isegye/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  isegye
+//
+//  Created by 손겸 on 2/25/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
